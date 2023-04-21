50b7291536a6cc5abb2e53bdd9ee921b8d7e373f
Server <server@gmail.com>
2023-04-21 23:24:33 +0400

    l

---
+++ server/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name'     => 'Admin',
+                'email'    => 'admin@admin.com',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name'     => 'Demo',
+                'email'    => 'demo@demo.com',
+                'password' => bcrypt('password'),
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
